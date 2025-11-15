40de55a5f8fac5a90c0970c741c8e34b3a79c998
Festivehiro <cewanghanphpps@gmail.com>
2025-11-15 10:14:17 +0800

    fixing up localised names for gear assembly @ raw rubber, and added more line of code for metaitemSTE

--- src/main/java/gregtech/common/items/MetaItemSTE.java
+++ src/main/java/gregtech/common/items/MetaItemSTE.java
@@ -1,5 +1,36 @@
 package gregtech.common.items;
 
+import gregtech.api.GTValues;
+import gregtech.api.items.OreDictNames;
+import gregtech.api.items.materialitem.MaterialMetaItem;
+import gregtech.api.items.metaitem.ElectricStats;
+import gregtech.api.items.metaitem.FluidStats;
+import gregtech.api.items.metaitem.FoodStats;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.unification.OreDictUnifier;
+import gregtech.api.unification.material.MarkerMaterials;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.material.type.Material;
+import gregtech.api.unification.material.type.SolidMaterial;
+import gregtech.api.unification.ore.OrePrefix;
+import gregtech.api.unification.stack.ItemMaterialInfo;
+import gregtech.api.unification.stack.MaterialStack;
+import gregtech.api.util.RandomPotionEffect;
+import gregtech.common.items.behaviors.*;
+import net.minecraft.block.BlockCauldron;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import static gregtech.common.items.MetaItems.GEAR_ASSEMBLY_LV;
 import static gregtech.common.items.MetaItems.GEAR_ASSEMBLY_MV;
 import static gregtech.common.items.MetaItems.GEAR_ASSEMBLY_HV;
